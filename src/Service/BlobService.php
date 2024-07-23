eb9465426e492601fad575777b23630a80be2fdb
Manuel Kocher <manuel.kocher@tugraz.at>
2024-07-23 15:42:43 +0200

    Add endpoint that returns the signed blob url

--- src/Service/BlobService.php
+++ src/Service/BlobService.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Dbp\Relay\CabinetBundle\Service;
 
 use Dbp\Relay\BlobLibrary\Api\BlobApi;
+use Dbp\Relay\CabinetBundle\Authorization\AuthorizationService;
+use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class BlobService implements LoggerAwareInterface
@@ -37,12 +41,18 @@ class BlobService implements LoggerAwareInterface
      */
     private $blobApi;
 
-    public function __construct(UrlGeneratorInterface $router)
+    /**
+     * @var AuthorizationService
+     */
+    private $auth;
+
+    public function __construct(UrlGeneratorInterface $router, AuthorizationService $auth)
     {
         $this->router = $router;
         $this->blobBaseUrl = '';
         $this->blobKey = '';
         $this->blobBucketId = '';
+        $this->auth = $auth;
     }
 
     public function setConfig(array $config)
@@ -51,5 +61,45 @@ class BlobService implements LoggerAwareInterface
         $this->blobKey = $config['blob_key'] ?? '';
         $this->blobBucketId = $config['blob_bucket_id'] ?? '';
         $this->blobApi = new BlobApi($this->blobBaseUrl, $this->blobBucketId, $this->blobKey);
+    }
+
+    public function getSignatureForGivenRequest(Request $request): Response
+    {
+        if (!$this->auth->isAuthenticated()) {
+            throw new ApiError(Response::HTTP_UNAUTHORIZED, 'access denied');
+        }
+
+        // Do basic authorization checks for the provided bearer token
+        // TODO: Check permissions
+        $this->auth->checkCanUse();
+
+        $method = 'POST';
+        $creationTime = rawurlencode((new \DateTime())->format('c'));
+
+        // get stuff from body
+        $prefix = $request->request->get('prefix', '');
+        $fileName = $request->request->get('fileName', '');
+        $retentionDuration = $request->request->get('retentionDuration', '');
+        $notifyEmail = $request->request->get('notifyEmail', '');
+        $type = $request->request->get('type', '');
+
+        try {
+            $params = [
+                'bucketIdentifier' => $this->blobBucketId,
+                'creationTime' => $creationTime,
+                'fileName' => $fileName,
+                'method' => $method,
+                'notifyEmail' => $notifyEmail,
+                'prefix' => $prefix,
+                'retentionDuration' => $retentionDuration,
+                'type' => $type,
+            ];
+
+            $responseUrl = $this->blobApi->getSignedBlobFilesUrl($params);
+
+            return new Response($responseUrl, 200);
+        } catch (\Exception $e) {
+            throw ApiError::withDetails(Response::HTTP_INTERNAL_SERVER_ERROR, 'The blob url could not be generated! Please check your parameters.', 'cabinet:cannot-generate-signed-blob-url');
+        }
     }
 }
