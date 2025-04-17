e878c7d795308db8a514eda6b7866a07503b0d10
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-17 15:51:06 +0200

    Add support for api-platform 4.1+

--- src/Blob/Dummy.php
+++ src/Blob/Dummy.php
@@ -6,6 +6,8 @@ namespace Dbp\Relay\CabinetBundle\Blob;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Parameter;
 
 #[ApiResource(
     shortName: 'cabinet',
@@ -14,60 +16,60 @@ use ApiPlatform\Metadata\Post;
         new Post(
             uriTemplate: '/blob-urls',
             controller: BlobSignatureController::class,
-            openapiContext: [
-                'tags' => ['Cabinet'],
-                'summary' => 'Returns a signed blob url which can be used to do operations on blob',
-                'parameters' => [
-                    [
-                        'name' => 'method',
-                        'in' => 'query',
-                        'description' => 'Blob method which will be used',
-                        'type' => 'string',
-                        'required' => true,
-                        'example' => 'GET',
-                    ],
-                    [
-                        'name' => 'type',
-                        'in' => 'query',
-                        'description' => 'Blob type which will be stored',
-                        'type' => 'string',
-                        'required' => false,
-                        'example' => '',
-                    ],
-                    [
-                        'name' => 'prefix',
-                        'in' => 'query',
-                        'description' => 'Prefix to be stored for the blob resource',
-                        'type' => 'string',
-                        'required' => false,
-                        'example' => '',
-                    ],
-                    [
-                        'name' => 'identifier',
-                        'in' => 'query',
-                        'description' => 'Identifier of the blob resource',
-                        'type' => 'string',
-                        'required' => false,
-                        'example' => '',
-                    ],
-                    [
-                        'name' => 'includeData',
-                        'in' => 'query',
-                        'description' => '1 if GET request should return base64 encoded filedata, not given otherwise',
-                        'type' => 'string',
-                        'required' => false,
-                        'example' => '',
-                    ],
-                    [
-                        'name' => 'deleteIn',
-                        'in' => 'query',
-                        'description' => 'Iso8601 duration in which the file should be deleted',
-                        'type' => 'string',
-                        'required' => false,
-                        'example' => 'P1D',
-                    ],
-                ],
-            ],
+            openapi: new Operation(
+                tags: ['Cabinet'],
+                summary: 'Returns a signed blob url which can be used to do operations on blob',
+                parameters: [
+                    new Parameter(
+                        name: 'method',
+                        in: 'query',
+                        description: 'Blob method which will be used',
+                        required: true,
+                        schema: ['type' => 'string'],
+                        example: 'GET'
+                    ),
+                    new Parameter(
+                        name: 'type',
+                        in: 'query',
+                        description: 'Blob type which will be stored',
+                        required: false,
+                        schema: ['type' => 'string'],
+                        example: ''
+                    ),
+                    new Parameter(
+                        name: 'prefix',
+                        in: 'query',
+                        description: 'Prefix to be stored for the blob resource',
+                        required: false,
+                        schema: ['type' => 'string'],
+                        example: ''
+                    ),
+                    new Parameter(
+                        name: 'identifier',
+                        in: 'query',
+                        description: 'Identifier of the blob resource',
+                        required: false,
+                        schema: ['type' => 'string'],
+                        example: ''
+                    ),
+                    new Parameter(
+                        name: 'includeData',
+                        in: 'query',
+                        description: '1 if GET request should return base64 encoded filedata, not given otherwise',
+                        required: false,
+                        schema: ['type' => 'string'],
+                        example: ''
+                    ),
+                    new Parameter(
+                        name: 'deleteIn',
+                        in: 'query',
+                        description: 'Iso8601 duration in which the file should be deleted',
+                        required: false,
+                        schema: ['type' => 'string'],
+                        example: 'P1D'
+                    ),
+                ]
+            ),
             normalizationContext: [
                 'groups' => ['Cabinet:output'],
                 'jsonld_embed_context' => true,
